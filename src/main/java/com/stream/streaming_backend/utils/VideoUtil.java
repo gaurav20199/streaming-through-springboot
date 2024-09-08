5829516621553eb9dbac22640cc0e68228da473c
Gaurav <gauravjoshi20199@gmail.com>
2024-09-09 02:33:08 +0530

    Added utility class for video related methods

---
+++ src/main/java/com/stream/streaming_backend/utils/VideoUtil.java
@@ -0,0 +1,29 @@
+package com.stream.streaming_backend.utils;
+
+import com.stream.streaming_backend.entities.Video;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import java.nio.file.Path;
+
+public class VideoUtil {
+
+    public static Resource getVideoResource(Video videoMetaData) {
+        String filePath = videoMetaData.getFilePath();
+        Path path = Path.of(filePath);
+        Resource resource = new FileSystemResource(path);
+        return resource;
+    }
+
+    public static HttpHeaders getHeadersForStreamingVideos() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Connection", "keep-alive");
+        headers.add("Cache-Control","no-cache, no-store, must-revalidate");
+        headers.add("Pragma","no-cache");
+        headers.add("Expires","0");
+        headers.add("X-Content-Type-Options","nosniff");
+        return headers;
+    }
+}
